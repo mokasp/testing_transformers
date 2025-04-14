1328b44d9978df259aba063c05650ca28956e090
mokasp <kasperbouldin@gmail.com>
2025-04-14 16:03:03 -0500

    (add) testing model

--- app.py
+++ app.py
@@ -13,6 +13,21 @@ def index():
     # 1. Load a pretrained Sentence Transformer model
     model = SentenceTransformer("all-MiniLM-L6-v2")
     logging.debug("model was loaded")
+
+    sentences = [
+    "The weather is lovely today.",
+    "It's so sunny outside!",
+    "He drove to the stadium.",
+    ]
+
+    # 2. Calculate embeddings by calling model.encode()
+    embeddings = model.encode(sentences)
+    logging.debug(str(embeddings.shape))
+    # [3, 384]
+
+    # 3. Calculate the embedding similarities
+    similarities = model.similarity(embeddings, embeddings)
+    logging.debug(str(similarities))
     return render_template("index.html")
 
 if __name__ == "__main__":
